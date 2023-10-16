5862ac17cf876d6ecae8b83fbd3aa663726dbab3
SvitlanaSuslenkova <136314004+SvitlanaSuslenkova@users.noreply.github.com>
2023-10-16 19:56:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
-
 const textError = document.getElementsByTagName("span")[0];
-const button = document.getElementsByTagName("button")[1];
-const section = document.getElementsByTagName("section");
-const input = document.getElementsByTagName("input")[0];
+const button1 = document.getElementsByTagName("button")[1];
+const section = document.getElementsByTagName("section")[3];
+const input = document.querySelector(".input");
+const main = document.getElementsByTagName("main")[0];
+
+
 
 function ValidateEmail(input) {
-  const result = input.value.trim();
   var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  if (result.match(validRegex) && result!=="") {
-    section[3].style.display='block';
-    input.classList.remove("inputError");
-    input.value="";
-    textError.style.display='none';
+const result = input.value.trim();
+
+
+ if (result.match(validRegex))  {
+   section.classList.replace("section4","displaySection4");
+   main.classList.add("noMain");
+   input.value = "your email was registered!";
+   document.form1.email.focus();
+   return true;
   }
   else {
-    textError.style.display='inline-block';
-    input.classList.add("inputError"); 
-  }}
+    textError.classList.add("spanDisplay");
+    input.classList.add("inputError");
+    document.form1.email.focus();
+    return false;
+}}
 
-  button.addEventListener("click", function() {
-    section[3].style.display='none';
+button1.addEventListener("click", function() {
+section.classList.replace("displaySection4","section4");
+input.value = "";
+main.classList.remove("noMain");
+input.classList.remove("inputError");
+textError.classList.remove("spanDisplay");
+});
 
-  
-  }
-  );
+
